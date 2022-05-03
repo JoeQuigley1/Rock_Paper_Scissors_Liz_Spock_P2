af457677ecedc9f5fa3b9283a6d7c865e6e958fb
JoeQuigley1 <joeyjtq@hotmail.com>
2022-05-03 19:47:02 +0000

    Add icons to button and remove color

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const buttons = document.getElementsByClassName("game-item");
+const playerScore = document.getElementById("score-you");
+const computerScore = document.getElementById("score-comp");

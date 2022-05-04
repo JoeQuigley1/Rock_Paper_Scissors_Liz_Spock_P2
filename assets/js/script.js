5e7a00a33e69c086b58a0ee5dd04f775d92d9abd
JoeQuigley1 <joeyjtq@hotmail.com>
2022-05-04 12:13:30 +0000

    Change layout of headings and score area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
 const buttons = document.getElementsByClassName("game-item");
 const playerScore = document.getElementById("score-you");
 const computerScore = document.getElementById("score-comp");
+
+
+function runGame(){
+
+}
+
+
+function incrementScorePlayer() {
+
+}
+
+function incrementScoreComputer() {
+    
+}

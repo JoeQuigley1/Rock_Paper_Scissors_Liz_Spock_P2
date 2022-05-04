21b26e25675d1fdebc9a182a99f9b9a3fa820849
JoeQuigley1 <joeyjtq@hotmail.com>
2022-05-04 16:01:18 +0000

    Add choices to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const buttons = document.getElementsByClassName("game-item");
 const playerScore = document.getElementById("score-you");
 const computerScore = document.getElementById("score-comp");
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
 
 function runGame(){

d35335277833cf299d182f837a8202baa398a2c4
JoeQuigley1 <joeyjtq@hotmail.com>
2022-05-07 10:38:52 +0000

    Add playGamefunction which call the image of the user choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,21 @@ const playerScore = document.getElementById("player-score");
 const computerScore = document.getElementById("computer-score");
 const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
+/**
+ * Add event listener to all buttons
+ */
+for(let button of buttons){
+    button.addEventListener("click", function(){
+        let playerChoice = this.getAttribute("data-choice")
+    });
+    console.log("clicked")
+}
 
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            let playerChoice = this.getAttribute("data-choice");
-            playGame(playerChoice);
+function playGame(){
 
-        });
- }
+    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
+    playerImage.alt = choices[playerChoice];
+}
 
 let computerChoice= Math.floor(Math.random() * 5);
 let rulesExplanation = document.getElementById("rules");

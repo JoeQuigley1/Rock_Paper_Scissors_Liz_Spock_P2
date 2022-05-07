ce81e168d9733124663be81195e58104130766a4
JoeQuigley1 <joeyjtq@hotmail.com>
2022-05-07 18:06:09 +0000

    Add function to change the player image when a button is clicked in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,27 @@ const computerImage = document.getElementById("computer-image");
 const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
 
+for (let button of buttons) {
+    button.addEventListener("click", function() {
+        let playerChoice = this.getAttribute("data-choice");
+        playGame(playerChoice);
+    });
+}
+
+function playGame(playerChoice) {
+
+    playerImage.src =`assets/images/${choices[playerChoice]}.png`;
+    computerImage.alt = choices[computerChoice];    
+
+    let computerChoice = Math.floor(Math.random() *3);
+
+    computerImage.src = `assets/images/${choices[computerChoice]}.png`;
+    computerImage.alt = choices[computerChoice];
+
+    let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+
+    updateScores(result);
+}
 
 function incrementScorePlayer() {
 

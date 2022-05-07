d0aa5bdaf21ab96a7c18e8659b0dd3d7c8a6afc2
JoeQuigley1 <joeyjtq@hotmail.com>
2022-05-07 17:55:51 +0000

    remove unused class button list and change header

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,7 @@ const playerImage = document.getElementById("player-image");
 const computerImage = document.getElementById("computer-image");
 const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
-buttons.forEach((button)  => {
-    buttons.addEventListener("click", function() {
-        pInput = this.textContent;
 
-    });
-});
 
 function incrementScorePlayer() {
 

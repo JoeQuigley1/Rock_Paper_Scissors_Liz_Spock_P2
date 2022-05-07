f2c1199846a6dfc82369daba47a805093cd1e32f
JoeQuigley1 <joeyjtq@hotmail.com>
2022-05-07 16:07:44 +0000

    revert bacl to previous style with images under player and computer add link to facebook in icon at footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
-const buttons = document.getElementsByClassName("game-item");
+const buttons = document.querySelectorAll(".game-item");
 const playerScore = document.getElementById("player-score");
 const computerScore = document.getElementById("computer-score");
+const playerImage = document.getElementById("player-image");
+const computerImage = document.getElementById("computer-image");
 const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
+buttons.forEach((button)  => {
+    buttons.addEventListener("click", function() {
+        pInput = this.textContent;
 
+    });
+});
 
 function incrementScorePlayer() {
 

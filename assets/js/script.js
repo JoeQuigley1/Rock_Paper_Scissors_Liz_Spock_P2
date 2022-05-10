fafa423a681230d02a4b9950da26649efeb0b1a6
JoeQuigley1 <joeyjtq@hotmail.com>
2022-05-10 16:45:32 +0000

    Add example modal to index CSS and JS.

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,6 +83,7 @@ function game(playerChoice) {
             break;
     }
     endGame();
+    
 
 }
 
@@ -101,15 +102,6 @@ function main() {
 }
 
 main();
-resetGame();
-
-function resetGame() {
-    resetButton.addEventListener('click',  () => {
-        console.log("Clicked");
-});
-}
-
-
 
 function endGame() {
     if (playerScore === 10){
@@ -124,4 +116,31 @@ function endGame() {
         computerScore = 0;
     }
     
+}
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }

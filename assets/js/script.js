440d4c4425911daa52279a83d88d4873e1ab8528
JoeQuigley1 <joeyjtq@hotmail.com>
2022-05-11 23:37:03 +0000

    Finish comments on JS script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Declare constants for DOM elements
 const playerScore_span = document.getElementById("player-score");
 const computerScore_span = document.getElementById("computer-score");
 const result_div = document.querySelector("#result");
@@ -10,19 +11,29 @@ let playerScore = 0;
 let computerScore = 0;
 const resetButton = document.getElementById("reset-button");
 
-
+/**
+ * Lists the choices
+ * and generates the computer choice by using a random number from the array of choices
+ */  
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
     const randomNumber =Math.floor(Math.random()*choices.length);
     return choices[randomNumber];
 }
+// Gets computer choice
 (getComputerChoice());
 
+// Capitalises each word in the result section
 function capitaliseWord(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-
+/**
+ * Finds the player score and increments it
+ * inserts the result string into the result section
+ * adds CSS styling to the icons once cliced
+ * removes the styling
+ */
 function win(playerChoice, computerChoice) {
     playerScore++;
     playerScore_span.innerHTML = playerScore;
@@ -32,6 +43,12 @@ function win(playerChoice, computerChoice) {
     setTimeout(function() {document.getElementById(playerChoice).classList.remove('winner'); }, 200);
 }
 
+/**
+ * Finds the computer score and increments it
+ * inserts the result string into the result section
+ * adds CSS styling to the icons once cliced
+ * removes the styling
+ */
 function lose(playerChoice, computerChoice) {
     computerScore++;
     playerScore_span.innerHTML = playerScore;
@@ -40,7 +57,7 @@ function lose(playerChoice, computerChoice) {
     document.getElementById(playerChoice).classList.add('loser');
     setTimeout(function() {document.getElementById(playerChoice).classList.remove('loser'); }, 200);
 }
-
+// Maintains the score and displays a string in the result
 function draw(playerChoice, computerChoice) {
     playerScore_span.innerHTML = playerScore;
     computerScore_span.innerHTML = computerScore;
@@ -49,6 +66,7 @@ function draw(playerChoice, computerChoice) {
     setTimeout(function() {document.getElementById(playerChoice).classList.remove('draw'); }, 200);
 }
 
+// Compares the all of the possible combinations and gives back either win lose or draw
 function game(playerChoice) {
     const computerChoice = getComputerChoice();
     switch(playerChoice + computerChoice) {
@@ -87,7 +105,7 @@ function game(playerChoice) {
     endGame();
 }
 
-
+// Add event listeners to all icons
 function main() {
     rock_div.addEventListener('click', () => game("rock"));
 
@@ -100,75 +118,68 @@ function main() {
     spock_div.addEventListener('click', () => game("spock"));
     
 }
-
 main();
 
+// Gives the game and end when either the user or computer gets to 10 points
 function endGame() {
     if (playerScore === 10){
         playerWin();
         playerScore = 0;
         computerScore = 0; 
-
     }
-   
     if (computerScore === 10){
         compWin();
         playerScore = 0;
         computerScore = 0;
         
     }
-    
 }
 
+// Styles the pop up message if the computer wins 
 function compWin() {
     document.getElementById('gameOver').style.display="block";
     document.getElementById('player-win').style.display ="none";
     document.getElementById('player-lose').style.display ="block";
 }
-
+// Styles the pop up message when the player wins
 function playerWin(){
     document.getElementById('gameOver').style.display ="block";
     document.getElementById('player-lose').style.display ="none";
     document.getElementById('player-win').style.display ="block";
 }
-
+// Rmoves the pop up messages and resets the score 
 function playAgain() {
     document.getElementById('gameOver').style.display="none";
     document.getElementById('player-win').style.display ="none";
     document.getElementById('player-lose').style.display ="none";
     resetScore();
 }
+// Gives the user the option to X out of the pop up without resetting score
 function giveUp() {
     document.getElementById('gameOver').style.display="block";
     document.getElementById('gameOver').style.display="none";
 }
-
-
-
-
-
-
-// Get the modal
+// Gets the modal
 const modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
+// Gets the button that opens the modal
 const btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
+// Gets the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
+// When the user clicks on the button, opens the modal
 btn.onclick = function() {
   modal.style.display = "block";
   console.log();
 };
 
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on <span> (x), closes the modal
 span.onclick = function() {
   modal.style.display = "none";
 };
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the modal, closes it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
